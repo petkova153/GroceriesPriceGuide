7feee77fb2a558d71adf984c996f312bdcefbc46
Evilyne <florina.gratiela@yahoo.com>
2023-10-06 20:56:21 +0300

    Product Class

--- src/main/java/com/groceriespriceguide/products/Product.java
+++ src/main/java/com/groceriespriceguide/products/Product.java
@@ -1,4 +1,30 @@
 package com.groceriespriceguide.products;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String productName;
+    private String productPrice;
+    private String store;
+    private String productCategory;
+    private String productUrl;
+    private String pictureUrl;
+    private Timestamp lastUpdated;
+
 }
