c813b133b1b7bc557068625cb2af45d78117ea07
petko <petkova153@gmail.com>
2023-10-09 21:40:52 +0300

    Correcting main and repository after resolving version issues

--- src/main/java/com/groceriespriceguide/GroceriesPriceGuideApplication.java
+++ src/main/java/com/groceriespriceguide/GroceriesPriceGuideApplication.java
@@ -2,8 +2,20 @@ package com.groceriespriceguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EntityScan(basePackages = {"com.groceriespriceguide.products.entity"})
+@ComponentScan({ "com.groceriespriceguide.scraper.scheduler",
+		"com.groceriespriceguide.scraper.scraper",
+		"com.groceriespriceguide.products.controller",
+		"com.groceriespriceguide.products.services.impl"
+})
+@EnableJpaRepositories(basePackages = "com.groceriespriceguide.products.repository")
 public class GroceriesPriceGuideApplication {
 
 	public static void main(String[] args) {
