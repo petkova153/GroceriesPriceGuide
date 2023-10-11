f0c29d0efef7950a79c31cc9bebb38203cfde5b1
karolinchen-parmas <karolina.parmas@protonmail.ch>
2023-10-11 19:34:21 +0300

    User class (23.10.09) WIP enddddd

--- src/main/java/com/groceriespriceguide/users/UserEntity.java
+++ src/main/java/com/groceriespriceguide/users/UserEntity.java
@@ -30,11 +30,9 @@ public class UserEntity {
 
     private Long id;
     private String username;
+    private String password;
     private String email;
     private String city;
-    private String favouriteProduct1;
-    private String favouriteProduct2;
-    private String favouriteProduct3;
     private Timestamp lastUpdated;
     private Timestamp createdAt;
     private Timestamp lastLoggedIn;
