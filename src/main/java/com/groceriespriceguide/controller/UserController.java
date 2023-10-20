b9fcc1f67a4617189610bd4d836f9df0a636f3ed
petko <petkova153@gmail.com>
2023-10-20 22:53:51 +0300

    response solved for remove

--- src/main/java/com/groceriespriceguide/controller/UserController.java
+++ src/main/java/com/groceriespriceguide/controller/UserController.java
@@ -90,8 +90,4 @@ public class UserController {
         response.addCookie(cookie);
         return "redirect:/login?status=LOGOUT_SUCCESS";
     }
-    @GetMapping("/favorites")
-    public String displayFavorites() {
-        return "favorites";
-    }
 }
